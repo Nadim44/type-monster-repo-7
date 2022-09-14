47d6f9fdbb1e7181a9e6ff95ff6ec888cff061f4
2Nadim <nadimmahamud661@gmail.com>
2022-09-14 16:32:32 +0100

    count down fixed

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
@@ -125,6 +125,9 @@ const start = () => {
 
 // START Countdown
 startBtn.addEventListener("click", start);
+// startBtn.addEventListener("click", function () {
+//   start()
+// });
 
 // If history exists, show it
 displayHistory();
